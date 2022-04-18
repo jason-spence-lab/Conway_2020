ed792a0fe0bbc9b72382670f7e970d435402b91f
Joshua Wu <wu.joshuah@gmail.com>
2022-04-18 16:32:10 -0400

    final revisions

--- analysis_script_updated.py
+++ analysis_script_updated.py
@@ -7,92 +7,210 @@ By Josh Wu
 Contains of Distal Lung for Renee Conway
 '''
 import sys
-sys.path.insert(0,'C:/Users/Josh/Desktop/sca/tools')
+sys.path.insert(0,'C:/Users/Josh/Desktop/sca_run/api/')
 #from scanpy_spence import *
-from sca_run import *
+from classes.sca_params import *
+import SCARunner as SCARunner
 
-figdir = './figures_052120/'
-an_run = sca_run()
+figdir = './figures_062921_dpt/'
 
-#############################################################################
-## Change this to point toward your mount location for our MiStorage share ##
-#############################################################################
-an_run.storage_mount_point = 'Z:/'
+an_params = sca_params()
+
+an_params.storage_mount_point = 'Z:/'
 
 ## IDs of samples as represented in the metadata table
-an_run.sample_list = ['2268-2','2598-33','2321-2','2288-4','2444-1'] # Distal Fetal Lung data
+# an_params.sample_list = ['2268-2','2598-33','2321-2','2288-4','2444-1'] # Distal Fetal Lung data
 
-## List of interesting genes
-an_run.add_gene_list(markers=['WNT2','WNT2B','WNT7B','RSPO1','RSPO3','RSPO4'],label='basic_list')
-	#['EPCAM','VIM','TCF21','CDH5','CD74','NEUROD1','TAGLN','RSPO2','FGFR4','WNT2'])
+an_params.sample_list = ['2288-4']
 
-an_run.add_gene_list(markers=['GPC3','CHST2','PIEZO2','ABCA8','SLC40A1','LGALS1','SLIT2','ITM2C','SLC1A5', 
-							  'BNIP3L','MMP23B','PLXDC2','APOE','GJA5','SCN7A','TMEM108','S1PR1','ABCA6', 
-							  'LIFR','ELN','MYLK','HHIP','MYLK','PLP1','BGN','SPARC','CD82','TYRP1', 
-							  'SERPINE2','WNT5A','KCNMB1','MRGPRF','PDGFC','KCNK17','MATN2','SVIL','AGTR2', 
-							  'WNT3A','PDGFRA','RSPO2','TAGLN','FGFR4','VIM','EPCAM','CD74','CDH5','WNT7B'],
-							  label='extended_list')
+an_params.add_gene_list(markers = ['EGR1','RSPO2','FGFR4','WNT2','CDO1','BMP5','LIFR',
+								   'COL1A1','COL1A2','ELN','SPARC','MGP','FBLN1','SOX11',
+								   'PDGFRA','PTCH1','HOXB5','HOXB4','NGFR','FGF7','BMP4',
+								   'TAGLN','ACTA2','MYL9','MYH11','ACTG2','HHIP','FOXF1',
+								   'WNT5A'], 
+						label='mesenchyme_list')
 
-an_run.add_gene_list(markers = ['RSPO2','FGFR4','WNT2','TAGLN','ACTA2','HHIP','PDGFRB',
-								'PDGFRA','RSPO1','RSPO3','RSPO4','LGR4','LGR5','LGR6',
-								'AXIN2','SOX9','SFTPC'],
-					 label = 'general_list')
+an_params.add_gene_list(markers = ['VIM','POSTN','DCN','TCF21','COL1A2','COL3A1','MKI67',
+								   'TOP2A','CDK1','TYMS','H2AFZ','STMN1','EPCAM','KRT18',
+								   'KRT8','CLDN6','TAGLN','ACTA2','PDGFRB','MGP','CDH5',
+								   'CLDN5','ESAM','KDR','FLT1','PTPRC','CD37','CORO1A',
+								   'LCP1','CD53','LAPTM5','S100B','ELAVL4','TUBB2B','STMN2',
+								   'ASCL1','NNAT','GRP','MPZ'],
+					 label='general_cell_type_list')
 
-an_run.add_gene_list(markers = ['RSPO2','FGFR4','WNT2','TAGLN','ACTA2','HHIP','PDGFRB',
-								'PDGFRA','RSPO1','RSPO3','RSPO4','LGR4','LGR6'],
-					 label = 'mesenchyme_list')
 
-an_run.add_gene_list(markers = ['SOX11','NGFR','PTCH1','HOXB5','HOXB4','BMP4','TGFB1','TGFB2',
-								'TGFB3','FOXF1','EGR1','TCF21','FN1','CDO1','BOC','PTCH1',
-								'SELL','FGF7','BMP5','MYL9','MYH11','ACTG2','PDGFRB','EGFR',
-								'NOTCH2','BMPR1A','BMPR2'],
-					 label = 'extracted_list')
+an_params.add_gene_list(markers = ['RGS13','RPS4Y1','CBLN2','APOC1','RPS4Y1','SOD3','CA3',
+								   'ANXA1','SDC2','STC1','SYT1','IGSF1','KCNK17','NKX3-2',
+								   'NTRK2','CHODL','CRYM','GATA2','PRSS23','SERPINF1'],
+						label='ranked_mesenchyme_list')
 
-an_run.add_gene_list(markers = ['EPCAM','CDH1','KRT18','KRT8','CLDN6','VIM','POSTN','DCN','TCF21',
-								 'COL1A2','COL3A1','COL1A2','TAGLN','ACTA2','PDGFRB','CD151','PTPRC','CD37',
-								 'CORO1A','LCP1','CD53','LAPTM5','CDH5','CLDN5','ESAM','KDR','FLT1','S100B',
-								 'ELAVL4','TUBB2B','STMN2','ASCL1','NNAT','GRP','MPZ'],
-					  label= 'gene_list_2')
+an_params.add_gene_list(markers = ['RPS4Y1','RSPO2','WNT2','CDO1','BMP5','LIFR','FGFR4','EGR1',
+								  'GUCA1A','CA3','SOX11','PDGFRA','PTCH1','HOXB4','SDC2','STC1',
+								  'MAPK14','KCNK17','ELN','MGP','BGN','PRSS23','SERPINF1','CRYM',
+								  'AGTR2','FGF7','BMP4','TAGLN','ACTA2','MYL9','MYH11','ACTG2',
+								  'HHIP','FOXF1','WNT5A'],
+					   label='updated_mesenchyme_3')
 
-an_run.add_gene_list(markers = ['FGF10','FOS','MKI67','TOP2A','CCND1','TYMS','DUT','CDK1','H2AFZ','STMN1','FAS','CASP3'],
-					  label='mesenchyme_extracted_list')
+an_params.add_gene_list(markers = ['EPCAM','KRT18','KRT8','CLDN6','VIM','POSTN','DCN','TCF21',
+								   'COL1A2','COL3A1','TAGLN','ACTA2','PDGFRB','RGS5','PTPRC',
+								   'CD37','CORO1A','LCP1','CD53','LAPTM5','CDH5','CLDN5',
+								   'ESAM','KDR','FLT1','S100B','ELAVL4','TUBB2B','STMN2',
+								   'ASCL1','NNAT','GRP','MPZ','MKI67','TOP2A','CDK1','TYMS',
+								   'H2AFZ','STMN1','LGR5'],
+						label='general_cell_type_list_updated')
+
+an_params.add_gene_list(markers = ['VIM','POSTN','DCN','TCF21','COL1A2','COL3A1','MKI67',
+								   'TOP2A','CDK1','TYMS','H2AFZ','STMN1','EPCAM','KRT18',
+								   'KRT8','CLDN6','TAGLN','ACTA2','PDGFRB','MGP','CDH5',
+								   'CLDN5','ESAM','KDR','FLT1','PTPRC','CD37','CORO1A',
+								   'LCP1','CD53','LAPTM5','S100B','ELAVL4','TUBB2B',
+								   'STMN2','ASCL1','NNAT','GRP','MPZ','CHGA','SYN1','LGR5'],
+						label='general_cell_type_list_explants')
+
+an_params.add_gene_list(markers = ['WNT1','WNT2','WNT2B','WNT3','WNT3A','WNT4','WNT5A',
+								   'WNT5B','WNT6','WNT7A','WNT7B','WNT8A','WNT8B','WNT9A',
+								   'WNT9B','WNT10A','WNT10B','WNT11','WNT16','DVL1','DVL2',
+								   'DVL3','HNF1A','TCF3','TCF4','LEF1','LRP5','LRP6',
+								   'FZD1','FZD2','FZD3','FZD4','FZD5','FZD6','FZD7','FZD8',
+								   'FZD9','FZD10','ANAPC1','APC2','DKK1','DKK2','DKK3',
+								   'DKK4','GSK3A','GSK3B','PORCN','RSPO1','RSPO2','RSPO3',
+								   'RSPO4','LGR4','LGR5','LGR6','RNF43','ZNRF3','AXIN2'],
+					 	label='WNT_gene_list')
+
+an_params.add_gene_list(markers = ['EGR1','RSPO2','FGFR4','WNT2','CDO1','FGF7','BMP5','BMP4',
+								   'LIFR','TAGLN','ACTA2','MYL9','MYH11','ACTG2','HHIP','FOXF1',
+								   'WNT5A','NGFR','SOX11','PDGFRA','PTCH1','HOXB5','HOXB4'],
+					    label='explant_mesenchyme_list')
+
+an_params.add_gene_list(markers = ['RSPO2','WNT2','CDO1','BMP5','LIFR','FGFR4','EGR1','TAGLN',
+								   'ACTA2','MYL9','MYH11','ACTG2','HHIP','FOXF1','WNT5A','ELN',
+								   'MGP','BGN','AGTR2','NGFR','FGF7','BMP4','SOX11','PDGFRA',
+								   'PTCH1','HOXB5','HOXB4'],
+						label='mesenchyme_list_updated2')
+
+an_params.add_gene_list(markers = ['SOX9','SFTPC','NPC2','TESC','CA2','ETV5','SOX2','TP63',
+								   'KRT5','KRT15','IL33','FOXL1','FOXJ1','CHGA','SYN1',
+								   'MUC5AC','MUC5B','FOXA3','SCGB1A1','BPIFA1','SCGB3A2',
+								   'CFTR','SFTPB','HOPX','PDPN','AQP5','AGER','ABCA3'],
+					 label='lung_epithelium_markers')
 
 ## Parameters used to filter the data - Mainly used to get rid of bad cells
-an_run.set_filter_params(min_cells = 0,
+an_params.set_qc_params(min_cells = 0,
 						min_genes = 750, # Filter out cells with fewer genes to remove dead cells
 						max_genes = 3000, # Filter out cells with more genes to remove most doublets
 						max_counts = 15000, # Filter out cells with more UMIs to catch a few remaining doublets
-						max_mito = 0.05) # Filter out cells with high mitochondrial gene content
+						max_mito = 5,
+						doublet_detection=False) # Filter out cells with high mitochondrial gene content
 
 ## Parameters used for initial clustering analysis
-an_run.set_analysis_params(n_neighbors = 30, # Size of the local neighborhood used for manifold approximation
-							n_pcs = 20, # Number of principle components to use in construction of neighborhood graph
+an_params.set_analysis_params(n_neighbors = 50, # Size of the local neighborhood used for manifold approximation
+							n_pcs = 30, # Number of principle components to use in construction of neighborhood graph
 							spread = 1, # In combination with min_dist determines how clumped embedded points are
 							min_dist = 0.4, # Minimum distance between points on the umap graph
-							resolution = 0.25,
-							remove_batch_effects = True) # High resolution attempts to increases # of clusters identified
+							resolution = 0.225,
+							clustering_choice = 'louvain',
+							do_bbknn = True) # High resolution attempts to increases # of clusters identified
 
-an_run.set_plot_params(size = 3,
-					   umap_obs = ['louvain','sampleName','age'],
-					   exp_grouping = ['louvain'])
+an_params.set_plot_params(size = 3,
+						   umap_obs = ['louvain','sampleName','age'],
+						   rank_grouping = ['louvain'],
+						   dot_grouping = ['louvain'])
 
 ## Basic pipeline for analysis - will filter data, process, cluster, etc. and output relevant figures
-# an_run.pipe_basic(figdir,load_save='adata_save.p')
+an_run = SCARunner.SCARunner()
+# an_run.pipe_basic(an_params,figdir,load_save='adata_save.p')
 
 ## If you find some interesting clusters that you want to "zoom in" on and recluster, you can use the following code
 # Will 
 # New analysis parameters for the subset of parameters
-analysis_params_ext = dict(n_neighbors = 15,
-						n_pcs = 10,
+an_params.set_analysis_params(n_neighbors = 30,
+						n_pcs = 20,
 						spread = 1,
 						min_dist = 0.4,
-						resolution = 0.4,
-						remove_batch_effects = True)
+						resolution = 0.55,
+						clustering_choice = 'louvain',
+						dpt = ['louvain',['0']],
+						do_bbknn = False,
+						draw_force_atlas = True,
+						phate=True,
+						umap_init_pos='paga')
 
-# an_run.pipe_ext(analysis_params_ext, figdir=figdir,load_save='adata_save.p',label='0_1_extracted',extracted=['0','1'])
+# an_run.pipe_ext(an_params, figdir=figdir,load_save='adata_save.p',label='3_extracted',extracted=['3'])
 
-an_run.pipe_ext(analysis_params_ext, figdir=figdir,load_save='adata_save.p',label='0_1_2_4_extracted',extracted=['0','1','2','4'])
+# an_run.pipe_ext(an_params, figdir=figdir,load_save='adata_save.p',label='0_1_2_4_extracted',extracted=['0','1','2','4'])
 
-an_run.clusters2_compare = ['all',['0','1','2']]
-an_run.pipe_ext(analysis_params_ext, figdir=figdir,load_save='adata_save.p',label='0_1_2_extracted',extracted=['0','1','2'])#, load_save='adata_save.p')
+# an_run.clusters2_compare = ['all',['0','1']]
+# an_run.pipe_ext(an_params, figdir=figdir,load_save='adata_save.p',label='0_1_extracted',extracted=['0','1'])
 
+# ##### Slightly adjusting qc parameters for cleaner dpt and force atlas graphs #####
+# import pickle
+# run_save = pickle.load(open(''.join([figdir,'/extracted/0_1_extracted/extracted_adata_save.p']),"rb"))
+# adata_postQC = run_save.adata_postQC[run_save.adata_postQC.obs['n_counts'] < 8000].copy()
+# # an_run.pipe_basic(an_params, adata_filtered = adata_postQC, figdir=''.join([figdir,'/extracted/0_1_extracted_reqc_bbknn/']))
+# an_params.annotation_dict = run_save.annotation_dict.copy()
+# # print(run_save.annotation_dict.copy())
+# for sample in an_params.sample_list:
+# 	sample_name = dict([i.split(':') for i in an_params.annotation_dict[sample][1:]])['sampleName']
+# 	adata_postQC_sub_sample = adata_postQC[adata_postQC.obs['sampleName']==sample_name].copy()
+# 	an_run.pipe_basic(an_params, adata_filtered = adata_postQC_sub_sample, figdir=''.join([figdir,'/extracted/0_1_',sample_name,'_reqc']))
+
+# an_run.pipe_basic(an_params, adata_filtered = adata_postQC.copy(), figdir=''.join([figdir,'/extracted/0_1_extracted_phate_bbknn/']))
+
+# an_params.analysis_params.do_bbknn = False
+# an_run.pipe_basic(an_params, adata_filtered = adata_postQC.copy(), figdir=''.join([figdir,'/extracted/0_1_extracted_phate/']))
+
+# an_run.pipe_basic(an_params, figdir=''.join([figdir,'/extracted/0_1_extracted/']), load_save='extracted_adata_save.p')
+
+##### Trying PHATE #####
+# import phate 
+# X_phate = phate.PHATE(n_jobs=-2).fit_transform(adata_postQC)
+# an_run.pipe_phate(an_params, figdir=figdir)
+
+## Parameters used to filter the data - Mainly used to get rid of bad cells
+an_params.set_qc_params(min_cells = 0,
+						min_genes = 750, # Filter out cells with fewer genes to remove dead cells
+						max_genes = 3000, # Filter out cells with more genes to remove most doublets
+						max_counts = 8000, # Filter out cells with more UMIs to catch a few remaining doublets
+						max_mito = 0.05,
+						doublet_detection=False) # Filter out cells with high mitochondrial gene content
+
+##### Trying CellRank
+## Parameters used for initial clustering analysis
+an_params.set_analysis_params(n_neighbors = 50, # Size of the local neighborhood used for manifold approximation
+							n_pcs = 30, # Number of principle components to use in construction of neighborhood graph
+							spread = 1, # In combination with min_dist determines how clumped embedded points are
+							min_dist = 0.4, # Minimum distance between points on the umap graph
+							resolution = 0.3,
+							clustering_choice = 'louvain',
+							do_bbknn = True) # High resolution attempts to increases # of clusters identified
+
+# an_run.pipe_basic(an_params,figdir=figdir,load_file_type='.loom', load_save='adata_save.p')
+
+an_params.set_analysis_params(n_neighbors = 30,
+						n_pcs = 20,
+						spread = 1,
+						min_dist = 0.4,
+						resolution = 0.55,
+						clustering_choice = 'louvain',
+						# dpt = ['louvain',['0']],
+						do_bbknn = False,
+						draw_force_atlas = False,
+						phate=False)
+						# umap_init_pos='paga')
+
+# an_run.pipe_ext(an_params, figdir=figdir,load_save='adata_save.p',label='0_1_extracted',extracted=['0','1'])
+
+figdir = './figures_070521_cellrank/'
+import pickle
+run_save = pickle.load(open(''.join([figdir,'/extracted/0_1_extracted/extracted_adata_save.p']),"rb"))
+adata_postQC = run_save.adata_postQC.copy()
+an_params.annotation_dict = run_save.annotation_dict.copy()
+# # del run_save
+# # an_run.pipe_cell_rank(an_params, adata_filtered=adata_postQC, figdir=figdir)
+# an_run.pipe_basic(an_params, adata_filtered=adata_postQC.copy(), figdir=''.join([figdir,'/extracted/0_2_extracted_reqc/']))
+
+for sample in an_params.sample_list:
+	sample_name = dict([i.split(':') for i in an_params.annotation_dict[sample][1:]])['sampleName']
+	adata_postQC_sub_sample = adata_postQC[adata_postQC.obs['sampleName']==sample_name].copy()
+	an_run.pipe_basic(an_params, adata_filtered=adata_postQC_sub_sample, figdir=''.join([figdir,'/extracted/0_1_',sample_name,'_cell_rank/']))
+	# an_run.pipe_cell_rank(an_params, adata_filtered=adata_postQC_sub_sample, figdir=''.join([figdir,'/extracted/0_1_',sample_name,'_cell_rank/']))
--- cultured_lung_script.py
+++ cultured_lung_script.py
@@ -12,131 +12,161 @@ In progress ---
 Moving to encapsulate parameters and relevant functions using class sca_set()
 '''
 import sys
-sys.path.insert(0,'C:/Users/Josh/Desktop/sca_run/sca_run')
+sys.path.insert(0,'C:/Users/Josh/Desktop/sca_run/sca_pipes')
+#from scanpy_spence import *
+from classes.sca_params import *
+import SCARunner as SCARunner
 
-from sca_run import *
-import csv
 #from tools.pipelines import *
 def run_analysis():
-	figdir = './figures_093020/'
-	an_run = sca_run()
+	figdir = './figures_071721/'
+	an_params = sca_params()
 	#############################################################################
 	## Change this to point toward your mount location for our MiStorage share ##
 	#############################################################################
-	an_run.storage_mount_point = 'Z:/'
+	an_params.storage_mount_point = 'Z:/'
 
 	# ## List of interesting genes
 
-	an_run.add_gene_list(markers = ['EPCAM','KRT18','KRT8','CLDN6','VIM','POSTN','DCN',
-									'TCF21','COL1A2','TAGLN','ACTA2','PDGFRB','PTPRC',
-									'CD37','CORO1A','LCP1','CD53','LAPTM5','CDH5','CLDN5',
-									'ESAM','KDR','FLT1','S100B','ELAVL4','TUBB2B','STMN2',
-									'NNAT','GRP','MPZ'], label='general_cell_types')
+	an_params.add_gene_list(markers = ['EGR1','RSPO2','FGFR4','WNT2','CDO1','BMP5','LIFR',
+									   'COL1A1','COL1A2','ELN','SPARC','MGP','FBLN1','SOX11',
+									   'PDGFRA','PTCH1','HOXB5','HOXB4','NGFR','FGF7','BMP4',
+									   'TAGLN','ACTA2','MYL9','MYH11','ACTG2','HHIP','FOXF1',
+									   'WNT5A'], 
+							label='mesenchyme_list')
 
-	an_run.add_gene_list(markers = ['EGR1','RSPO2','FGFR4','WNT2','CDO1','FGF7','BMP5',
-									'BMP4','MYL9','MYH11','ACTG2','HHIP','FOXF1','WNT5A',
-									'MKI67','TOP2A','CDK1','TYMS','H2AFZ','STMN1','SOX11',
-									'NGFR','PDGFRA','PTCH1','HOXB5','HOXB4'],
-						 label='mesenchyme_markers')
+	an_params.add_gene_list(markers = ['VIM','POSTN','DCN','TCF21','COL1A2','COL3A1','MKI67',
+									   'TOP2A','CDK1','TYMS','H2AFZ','STMN1','EPCAM','KRT18',
+									   'KRT8','CLDN6','TAGLN','ACTA2','PDGFRB','MGP','CDH5',
+									   'CLDN5','ESAM','KDR','FLT1','PTPRC','CD37','CORO1A',
+									   'LCP1','CD53','LAPTM5','S100B','ELAVL4','TUBB2B','STMN2',
+									   'ASCL1','NNAT','GRP','MPZ'],
+						 label='general_cell_type_list')
 
-	an_run.add_gene_list(markers = ['RSPO1','RSPO3','RSPO4','LGR4','LGR5','LGR6','AXIN2'],
-						 label='RSPO_list')
+	an_params.add_gene_list(markers = ['VIM','POSTN','DCN','TCF21','COL1A2','COL3A1','MKI67',
+									   'TOP2A','CDK1','TYMS','H2AFZ','STMN1','EPCAM','KRT18',
+									   'KRT8','CLDN6','TAGLN','ACTA2','PDGFRB','MGP','CDH5',
+									   'CLDN5','ESAM','KDR','FLT1','PTPRC','CD37','CORO1A',
+									   'LCP1','CD53','LAPTM5','S100B','ELAVL4','TUBB2B',
+									   'STMN2','ASCL1','NNAT','GRP','MPZ','CHGA','SYN1','LGR5'],
+							label='general_cell_type_list_explants')
 
-	an_run.add_gene_list(markers = ['SOX9','SFTPC','NPC2','TESC','CA2','ETV5','SOX2','TP63',
-									'KRT5','KRT15','IL33','FOXI1','FOXJ1','CHGA','SYN1',
-									'MUC5AC','MUC5B','FOXA3','SCGB1A1','BPIFA1',
-									'SCGB3A2','CFTR','SFTPB','HOPX','PDPN','AQP5',
-									'AGER','ABCA3'],
-						 label='epithelial_markers')
+	an_params.add_gene_list(markers = ['WNT1','WNT2','WNT2B','WNT3','WNT3A','WNT4','WNT5A',
+									   'WNT5B','WNT6','WNT7A','WNT7B','WNT8A','WNT8B','WNT9A',
+									   'WNT9B','WNT10A','WNT10B','WNT11','WNT16','DVL1','DVL2',
+									   'DVL3','HNF1A','TCF3','TCF4','LEF1','LRP5','LRP6',
+									   'FZD1','FZD2','FZD3','FZD4','FZD5','FZD6','FZD7','FZD8',
+									   'FZD9','FZD10','ANAPC1','APC2','DKK1','DKK2','DKK3',
+									   'DKK4','GSK3A','GSK3B','PORCN','RSPO1','RSPO2','RSPO3',
+									   'RSPO4','LGR4','LGR5','LGR6','RNF43','ZNRF3','AXIN2'],
+						 	label='WNT_gene_list')
+
+	an_params.add_gene_list(markers = ['EGR1','RSPO2','FGFR4','WNT2','CDO1','FGF7','BMP5','BMP4',
+									   'LIFR','TAGLN','ACTA2','MYL9','MYH11','ACTG2','HHIP','FOXF1',
+									   'WNT5A','NGFR','SOX11','PDGFRA','PTCH1','HOXB5','HOXB4'],
+						    label='explant_mesenchyme_list')
+
+	an_params.add_gene_list(markers = ['SOX9','SFTPC','NPC2','TESC','CA2','ETV5','SOX2','TP63',
+									   'KRT5','KRT15','IL33','FOXL1','FOXJ1','CHGA','SYN1',
+									   'MUC5AC','MUC5B','FOXA3','SCGB1A1','BPIFA1','SCGB3A2',
+									   'CFTR','SFTPB','HOPX','PDPN','AQP5','AGER','ABCA3'],
+						 label='lung_epithelium_markers')
+
+	# cell_score_lists = ['BTP_short','BTP_long','basal_score',
+ #   					    'budtipadj_score','club_score','goblet_score',
+ #   					    'int_ciliated_score','multiciliated_score',
+ #   					    'neuroendocrine_score','multiciliated_pre_score',
+ #   					    'secretory_progenitor_score','submucosa_score',
+ #   					    'submucosa_basal_score','basal_full','budtipadj_full',
+ #   					    'club_full','goblet_full','int_ciliated_full','multiciliated_full',
+	# 					'neuroendocrine_full','multiciliated_pre_full',
+	# 					'secretory_progenitor_full','submucosa_full',
+	# 					'submucosa_basal_full','WNT_CellScore']
+
+	cell_score_lists = ['Multiciliated_CellScore_LogFold','SecretoryProgenitor_CellScore_LogFold',
+						'SubmucosaBasal_CellScore_LogFold','MulticiliatedPrecursor_CellScore_LogFold',
+						'BudTipProgenitor_CellScore_LogFold','BudTipAdjacent_CellScore_LogFold',
+						'IntermediateCiliated_CellScore_LogFold','BasalCell_CellScore_LogFold',
+						'Submucosa_CellScore_LogFold','GobletCell_CellScore_LogFold','ClubCell_CellScore_LogFold']
+	
+	for score_list in cell_score_lists:
+   		an_params.add_gene_list(load_file=score_list, label=score_list, cell_score_list='Only')
 
 	## Parameters used for initial clustering analysis
-	an_run.set_analysis_params(n_neighbors = 20, # Size of the local neighborhood used for manifold approximation
-							   n_pcs = 15, # Number of principle components to use in construction of neighborhood graph
+	an_params.set_analysis_params(n_neighbors = 30, # Size of the local neighborhood used for manifold approximation
+							   n_pcs = 20, # Number of principle components to use in construction of neighborhood graph
 							   spread = 1, # In combination with min_dist determines how clumped embedded points are
-							   min_dist = 0.4, # Minimum distance between points on the umap graph
-							   resolution = 0.4, # High resolution attempts to increases # of clusters identified
-							   cell_score_lists = ['BTP_short','BTP_long','basal_score',
-							   					   'budtipadj_score','club_score','goblet_score',
-							   					   'int_ciliated_score','multiciliated_score',
-							   					   'neuroendocrine_score','multiciliated_pre_score',
-							   					   'secretory_progenitor_score','submucosa_score',
-							   					   'submucosa_basal_score'])
+							   min_dist = 0.4, # Minimum distance between points on the umap graph.
+							   clustering_choice = 'louvain',
+							   resolution = 0.4) # High resolution attempts to increases # of clusters identified
 
 	## Parameters used to filter the data - Mainly used to get rid of bad cells
-	an_run.set_filter_params(min_cells = 0, # Filter out cells 
-							 min_genes = 500, # Filter out cells with fewer genes to remove dead cells
+	an_params.set_qc_params(min_cells = 0, # Filter out cells 
+							 min_genes = 750, # Filter out cells with fewer genes to remove dead cells
 							 max_genes = 10000, # Filter out cells with more genes to remove most doublets
 							 max_counts = 50000, # Filter out cells with more UMIs to catch a few remaining doublets
 							 max_mito = 0.1) # Filter out cells with high mitochondrial gene content
 
-	an_run.set_plot_params(umap_obs = ['louvain','sampleName'],
-						   exp_grouping = ['louvain'],
+	an_params.set_plot_params(umap_obs = ['louvain','sampleName'],
+						   dot_grouping = ['louvain'],
 						   rank_grouping = ['louvain','sampleName'],
 						   size=5)#,
 						   # final_quality=True)
+	an_run = SCARunner.SCARunner()
 
-	# an_run.sample_list=['283-1','283-2','283-3']
-	# an_run.pipe_basic(''.join([figdir,'all/']))#,load_save='adata_save.p')
+	an_params.sample_list=['283-1','283-2','283-3']
+	# an_run.pipe_basic(an_params,''.join([figdir,'all/']))#,load_save='adata_save.p')
 
-	# an_run.sample_list=['283-2','283-3']
-	# an_run.pipe_basic(''.join([figdir,'control_+_lgd_ecd/']))#,load_save='adata_save.p')
+	an_params.sample_list=['283-2','283-3']
+	# an_run.pipe_basic(an_params,''.join([figdir,'control_+_lgd_ecd/']))#,load_save='adata_save.p')
 
-	# an_run.rank_grouping = ['louvain']
+	# an_params.plot_params.rank_grouping = ['louvain']
 
-	# an_run.sample_list=['283-1']
-	# an_run.pipe_basic(''.join([figdir,'no_virus/']))#,load_save='adata_save.p')
+	# an_params.sample_list=['283-1']
+	# an_run.pipe_basic(an_params,''.join([figdir,'no_virus/']))#,load_save='adata_save.p')
 
-	# an_run.sample_list=['283-2']
-	# an_run.pipe_basic(''.join([figdir,'control_virus/']))#,load_save='adata_save.p')
+	# an_params.sample_list=['283-2']
+	# an_run.pipe_basic(an_params,''.join([figdir,'control_virus/']))#,load_save='adata_save.p')
 
-	# an_run.sample_list=['283-3']
-	# an_run.pipe_basic(''.join([figdir,'lgd_ecd/']))#,load_save='adata_save.p')
+	# an_params.sample_list=['283-3']
+	# an_run.pipe_basic(an_params,''.join([figdir,'lgd_ecd/']))#,load_save='adata_save.p')
 	
 	## If you find some interesting clusters that you want to "zoom in" on and recluster, you can use the following code
 	# New analysis parameters for the subset of parameters
-	analysis_params_ext = dict(n_neighbors = 15,
-							n_pcs = 11,
-							spread = 1,
-							min_dist = 0.4,
-							resolution = 0.4,
-							cell_score_lists = ['BTP_short','BTP_long','basal_score',
-						   					    'budtipadj_score','club_score','goblet_score',
-						   					    'int_ciliated_score','multiciliated_score',
-						   					    'neuroendocrine_score','multiciliated_pre_score',
-						   					    'secretory_progenitor_score','submucosa_score',
-						   					    'submucosa_basal_score','basal_full','budtipadj_full',
-						   					    'club_full','goblet_full','int_ciliated_full','multiciliated_full',
-												'neuroendocrine_full','multiciliated_pre_full',
-												'secretory_progenitor_full','submucosa_full',
-												'submucosa_basal_full'])
+	an_params.set_analysis_params(n_neighbors = 20,
+								n_pcs = 15,
+								spread = 1,
+								min_dist = 0.4,
+								resolution = 0.4,
+								clustering_choice = 'louvain',)
 
-	an_run.size=20
-	an_run.rank_grouping = ['louvain','sampleName']
-	# an_run.pipe_ext(analysis_params_ext, figdir=''.join([figdir,'all/']), label='epithelium',
-	#  			extracted=['2'], load_save='adata_save.p')
+	an_params.plot_params.size=20
+	an_params.plot_params.rank_grouping = ['louvain','sampleName']
+	# an_run.pipe_ext(an_params, figdir=''.join([figdir,'all/']), label='epithelium_1_8',
+	#  			extracted=['1','8'], load_save='adata_save.p')
 
-	# an_run.pipe_ext(analysis_params_ext, figdir=''.join([figdir,'control_+_lgd_ecd/']), label='epithelium',
-	#  			extracted=['1'], load_save='adata_save.p')
+	# an_run.pipe_ext(an_params, figdir=''.join([figdir,'control_+_lgd_ecd/']), label='epithelium_1_8',
+	#  			extracted=['1','8'], load_save='adata_save.p')
 
-	an_run.pipe_ext(analysis_params_ext, figdir=''.join([figdir, 'control_+_lgd_ecd/extracted/epithelium/']),
-					extracted=['0'], load_save='adata_save.p')
+	an_run.pipe_ext(an_params, figdir=''.join([figdir, 'control_+_lgd_ecd/extracted/epithelium_1_8/']),
+					extracted=['0'], label='c0', load_save='extracted_adata_save.p')
 
 	# ## Violin plots for filtering parameters pre and post
-	sc.pl.violin(an_run.adata, an_run.gene_dict['epithelial_markers']['markers'],groupby='louvain',
-				 jitter=0.4, multi_panel=True, save='_epithelial_louvain.png', show=False)
+	# sc.pl.violin(an_params.adata, an_params.gene_dict['epithelial_markers']['markers'],groupby='louvain',
+	# 			 jitter=0.4, multi_panel=True, save='_epithelial_louvain.png', show=False)
 
-	sc.pl.violin(an_run.adata, an_run.gene_dict['epithelial_markers']['markers'],groupby='sampleName',
-				 jitter=0.4, multi_panel=True, save='_epithelial_sample.png', show=False)
+	# sc.pl.violin(an_params.adata, an_params.gene_dict['epithelial_markers']['markers'],groupby='sampleName',
+	# 			 jitter=0.4, multi_panel=True, save='_epithelial_sample.png', show=False)
 
-	# an_run.rank_grouping = ['louvain']
-	# an_run.pipe_ext(analysis_params_ext, figdir=''.join([figdir,'no_virus/']), label='epithelium',
+	# an_params.plot_params.rank_grouping = ['louvain']
+	# an_run.pipe_ext(an_params, figdir=''.join([figdir,'no_virus/']), label='epithelium_3_5_6',
 	#  			extracted=['3','5','6'], load_save='adata_save.p')
 
-	# an_run.pipe_ext(analysis_params_ext, figdir=''.join([figdir,'control_virus/']), label='epithelium',
+	# an_run.pipe_ext(an_params, figdir=''.join([figdir,'control_virus/']), label='epithelium_2',
 	#  			extracted=['2'], load_save='adata_save.p')
 
-	# an_run.pipe_ext(analysis_params_ext, figdir=''.join([figdir,'lgd_ecd/']), label='epithelium',
-	#  			extracted=['2'], load_save='adata_save.p')
+	# an_run.pipe_ext(an_params, figdir=''.join([figdir,'lgd_ecd/']), label='epithelium_2_7',
+	#  			extracted=['2','7'], load_save='adata_save.p')
 
 							
 if __name__ == "__main__":
